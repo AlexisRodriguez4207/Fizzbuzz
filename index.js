b4c2b1f7226b57a480770aa7c388b34b1fdeae77
Xero4207 <carlos.rodriguez201@tectijuana.edu.mx>
2022-05-12 10:23:16 -0700

    Creacion de Reader y ExplorerServices

---
+++ index.js
@@ -0,0 +1,10 @@
+const Reader = require("./lib/utils/Reader")
+const ExplorerService = require("./lib/Services/ExplorerServices")
+
+const explorers = Reader.readJsonFile("explorers.json")
+//console.log(explorers)
+
+// Aplicación del ExplorerService sobre la lista de explorers
+console.log(ExplorerService.filterByMission(explorers, "node"));
+//ExplorerService.getAmountOfExplorersByMission(explorers, "node");
+//ExplorerService.getExplorersUsernamesByMission(explorers, "node");
